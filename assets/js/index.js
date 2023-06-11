6cab4854c296fedc5c1d66261ca6d2281c0478f4
Wallace Vilela <wallace.vilela9@gmail.com>
2023-06-11 19:40:06 -0300

    Iniciando a parte lógica do projeto com javascript

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+let myForm = document.querySelector(".form");
+let respDay = document.querySelecto(".result-days");
+let respMonth = document.querySelecto(".result-months");
+let respYear = document.querySelecto(".result-years");
